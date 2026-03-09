26be4b0f7d358e20689565248ad5b744438bbc46
PakZek <peterlouis66@gmail.com>
2026-03-09 06:16:07 +0100

    Update jdm_hunter.py

--- jdm_hunter.py
+++ jdm_hunter.py
@@ -7,8 +7,8 @@ import re
 import json
 
 YEN_TO_EUR = 1 / 180
-BUDGET_UE_MAX = 40000
-BUDGET_IMPORT_MAX = 35000
+BUDGET_UE_MAX = 50000
+BUDGET_IMPORT_MAX = 45000
 
 MODELES = {
     "eclipse_gsx":  {"priorite": 1, "prix_moyen": 22000},
